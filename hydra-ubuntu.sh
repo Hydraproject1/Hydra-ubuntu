b0171bd92a58a9f108867782179c7604668bf741
Hydraproject1 <46571319+Hydraproject1@users.noreply.github.com>
2019-01-20 15:17:18 +0100

    Add files via upload

---
+++ hydra-ubuntu.sh
@@ -0,0 +1,112 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# this's script created in 19/01/2019
+# created by Hydra_Project
+
+setterm -foreground red
+echo "        #####################################"
+echo "        #                                   #"
+echo "        #     created by : Hamza Mhamdi     #"
+echo "        #           HYDRA_PROJECT           #"
+echo "        #                                   #"
+echo "        #####################################"
+sleep 3
+
+dpkg --configure -a
+apt update -y
+apt upgrade -y
+
+sleep 1
+setterm -foreground blue
+apt install proot -y
+apt install git -y
+apt install wget -y
+apt install -y util-linux;apt install figlet
+sleep 2
+
+setterm -foreground blue
+cd $HOME
+git clone https://github.com/Neo-Oli/termux-ubuntu
+cd termux-ubuntu
+sleep 2
+setterm -foreground yellow
+echo "      Hydra";
+echo ""
+echo ""
+sleep 1
+echo "               Will   ";
+echo ""
+echo ""
+sleep 1
+echo "                       Change ";
+echo ""
+echo ""
+sleep 1
+echo "                                 youre ";
+echo ""
+echo ""
+sleep 1
+echo "                                        life";
+echo ""
+echo "";
+sleep 2
+echo "+-----------------------------------------------+"
+echo "| Hydra_Project installed Ubuntu in youre Termux|"
+echo "+-----------------------------------------------+"
+sleep 2
+sh ubuntu.sh
+
+
+setterm -foreground red
+echo "      #####################################"
+setterm -foreground blue
+echo "      #  Hydra Will be started ubuntu now :"
+setterm -foreground red
+echo "      #####################################"
+sleep 2
+
+setterm -foreground green
+echo "start ubuntu [Loading whait]"
+sleep 1
+echo "start ubuntu [............]"
+sleep 1
+echo "start ubuntu [#...........]";
+sleep 1
+echo "start ubuntu [##..........]";
+sleep 1
+echo "start ubuntu [###.........]";
+sleep 1
+echo "start ubuntu [####........]";
+sleep 1
+echo "start ubuntu [#####.......]";
+sleep 1
+echo "start ubuntu [######......]";
+sleep 1
+echo "start ubuntu [#######.....]";
+sleep 1
+echo "start ubuntu [########....]";
+sleep 1
+echo "start ubuntu [#########...]";
+sleep 1
+echo "start ubuntu [##########..]";
+sleep 1
+echo "start ubuntu [###########.]";
+sleep 1
+echo "start ubuntu [############]";
+sleep 1
+echo "start ubuntu [starting-ubuntu]";
+sleep 2
+clear
+setterm -foreground green
+echo "          subscribe to my channel for more tools"
+echo ""
+echo "                      channel link "
+echo ""
+echo "https://www.youtube.com/channel/UCHHlpNSr1n121wfvabPg6rQ"
+echo ""
+sleep 5
+clear
+
+setterm -foreground red
+
+./start-ubuntu.sh
+
